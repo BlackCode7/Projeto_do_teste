2a033163177fce3f980337d247b0381a3557f07e
BlackCode7 <Black7Code@outlook.com>
2020-12-18 13:46:57 -0300

    Criando um novo diretório client2

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <nav>
+      <div class="nav-wrapper blue darken-1">
+        <a href="#" class="brand-logo center">Cadastro Livros</a>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <form>
+
+          <label>Id</label>
+          <input type="number" placeholder="Id">
+          <label>Nome</label>
+          <input type="text" placeholder="Nome">
+          <label>Autor</label>
+          <input type="text" placeholder="Autor">
+          <label>Lido</label>
+          <input type="text" placeholder="Já leu o livros? sim / não">
+
+          <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
+
+      </form>
+
+      <table>
+
+        <thead>
+
+          <tr>
+            <th>ID</th>
+            <th>NOME DO LIVRO</th>
+            <th>NOME DO AUTOR</th>
+            <th>LIDO</th>
+          </tr>
+
+        </thead>
+
+        <tbody>
+
+          <tr>
+
+            <td>Id</td>
+            <td>Nome</td>
+            <td>Autor</td>
+            <td>Lido</td>
+            <td>
+              <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
+              <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete</i></button>
+            </td>
+
+          </tr>
+
+        </tbody>
+      
+      </table>
+
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Livro from './services/Livros'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  
+  mounted(){
+    Livro.listar().then(resposta => {
+      console.log(resposta)
+    })
   }
+
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
